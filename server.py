be5ac21dee65fc67436439ad8f19b53edd34dc6e
Zyrome <Zyrome@ya.ru>
2019-03-11 15:43:01 +0400

    added db.py and users.db

---
+++ server.py
@@ -0,0 +1,58 @@
+##import sqlite3
+## 
+## 
+##class DB:
+##    def __init__(self):
+##        conn = sqlite3.connect('users.db', check_same_thread=False)
+##        self.conn = conn
+## 
+##    def get_connection(self):
+##        return self.conn
+## 
+##    def __del__(self):
+##        self.conn.close()
+##
+##
+##class UsersModel:
+##    def __init__(self, connection):
+##        self.connection = connection
+##
+##    def init_table(self):
+##        cursor = self.connection.cursor()
+##        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
+##                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+##                             user_name VARCHAR(50),
+##                             password_hash VARCHAR(128)
+##                             )''')
+##        cursor.close()
+##        self.connection.commit()
+##
+##    def insert(self, user_name, password_hash):
+##        if any([user_name == name for name in map(lambda x: x[1], self.get_all())]):
+##            return False
+##        cursor = self.connection.cursor()
+##        cursor.execute('''INSERT INTO users 
+##                          (user_name, password_hash) 
+##                          VALUES (?,?)''', (user_name, password_hash))
+##        cursor.close()
+##        self.connection.commit()
+##        return True
+##
+##    def get(self, user_id):
+##        cursor = self.connection.cursor()
+##        cursor.execute("SELECT * FROM users WHERE id = ?", (str(user_id),))
+##        row = cursor.fetchone()
+##        return row
+##     
+##    def get_all(self):
+##        cursor = self.connection.cursor()
+##        cursor.execute("SELECT * FROM users")
+##        rows = cursor.fetchall()
+##        return rows
+##
+##
+##db = DB()
+##nm = UsersModel(db.get_connection())
+##nm.init_table()
+##print(nm.insert('user', 'pass'))
+##print(nm.get_all())
