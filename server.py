2dfed8aa5820b3893c0790fa07d18afb725b8acd
TrueProgrammer <sasfomin@gmail.com>
2019-04-02 18:49:53 +0500

    Search page

--- server.py
+++ server.py
@@ -323,6 +323,70 @@ def download(folder, file):
 ##    return redirect('/drive')
 
 
+
+@app.route('/drivesearch')
+def drivesearch():
+    return '''<head>
+                        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                        <style>
+                            .litt{
+                                margin-left: 15px;
+                            }
+                            #footer {
+                                position: fixed;
+                                left: 0; 
+                                bottom: 0;
+                                color: #fff;
+                                width: 100%;
+                                height: 5%;
+                           }
+                           #footer div {
+                                padding: 10px;
+                                background: rgba(52, 58, 64, 1); 
+                           }
+                           .bige{
+                                font-size: 40px;
+                                margin-left: 160px;
+                                margin-top: 100px;
+                           }
+                           .biger{
+                                font-size: 20px;
+                                margin-left: 165px;
+                                margin-top: 10px;
+                           }
+                        </style>
+                    </head>
+                    <body class="bg-secondary">
+                        <nav class="navbar fixed-top navbar-dark bg-dark">
+                            <span><a href="http://127.0.0.1:8080/drive"><img src="static/img/LogoDarkUndStilished.png" width="130" height="50" alt="ERROR"></a></span>
+                            <span class="navbar-text"></span>
+                        </nav>
+                        </br></br></br>
+                        <p>
+                        <img width="10%" height="20%" class="bige" alt="ERROR" src="static/img/fileIcon.png">
+                        </p>
+                        <p>
+                            <div class="dropdown">
+                                <a class="nav-link dropdown-toggle text-white biger" id="{0}" role="button" aria-haspopup="true" aria-expanded="false"  data-toggle="dropdown"  href="#" >FILE NAME</a>
+                                <div class="dropdown-menu bg-secondary text-white" aria-labelledby="{0}">
+                                  <a class="dropdown-item bg-secondary text-white" href="/download/nodir/{0}">Скачать</a>
+                                  <a class="dropdown-item bg-secondary text-white" href="/delete/nodir/{0}">Удалить</a>
+                                </div>
+                            </div>
+                        </p>
+                        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                        <div id="footer">
+                       <div>
+                        &copy; Alex(GameTrue) & Артем(Zyrome64)
+                       </div>
+                      </div>
+                    </body>
+
+                        '''
+
+
 @app.route('/404')
 def not_found_error():
     return '''<head>
